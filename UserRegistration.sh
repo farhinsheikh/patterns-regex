cd743e2ef56e17da01c1d1f433e5f55a4815ef2b
Farhin Sheikh <farhinsheikh2015@gmail.com>
2020-06-06 17:31:22 +0530

    password has minimum 8 charecters

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -1,14 +1,13 @@
 echo "welcome to user registration program on master branch"
 
 #!/bin/bash -x
-echo "enter mobile number"
-read number
-pat="^([0-9]{2}[ ]?)[0-9]{10}$"
+echo "enter password"
+read password
+pat="^[a-zA-Z]{8,}$"
 
-if [[ $number =~ $pat ]];
+if [[ $password =~ $pat ]];
 then
         echo "valid"
 else
         echo "invalid"
 fi
-
