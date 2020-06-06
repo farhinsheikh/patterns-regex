8b020068033a5c807d9857205be4a54b7567b3c9
Farhin Sheikh <farhinsheikh2015@gmail.com>
2020-06-06 17:14:48 +0530

    first name

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -1,2 +1,15 @@
 echo "welcome to user registration program on master branch"
 
+#!/bin/bash -x
+echo "enter first name"
+read name
+pat="^[A-Za-z]{3,}$"
+
+if [[ $name =~ $pat ]];
+then
+   echo "valid"
+else
+   echo "invalid"
+fi
+
+
