77f7d79444357265ccd997e7adf1e86d0749774d
Farhin Sheikh <farhinsheikh2015@gmail.com>
2020-06-06 17:23:55 +0530

    check valid email

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -1,15 +1,13 @@
 echo "welcome to user registration program on master branch"
 
 #!/bin/bash -x
-echo "enter last name"
-read name
-pat="^[a-zA-Z]{3,}$"
+echo "enter email"
+read email
+pat="^([a-zA-Z0-9])+[._-+]*[0-9a-zA-Z]*[@][a-zA-Z]*([.][a-zA-Z]+){1,2}$"
 
-if [[ $name =~ $pat ]];
+if [[ $email =~ $pat ]];
 then
         echo "valid"
 else
         echo "invalid"
 fi
-
-
